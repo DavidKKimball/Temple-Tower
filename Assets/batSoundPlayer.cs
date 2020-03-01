9727f6d1b699b90ee5fc2347ba62a2f60ee76631
DK <davidkkimball@gmail.com>
2020-03-01 12:48:43 -0500

    adding sounds

---
+++ Assets/batSoundPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class batSoundPlayer : MonoBehaviour
+{
+    private AudioSource batSqueek;
+    public float squeekDelay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        batSqueek =  GetComponent<AudioSource>();
+        StartCoroutine(batSqueekPlay());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator batSqueekPlay()
+    {
+        yield return new WaitForSeconds(squeekDelay);
+        batSqueek.Play();
+    }
+}
