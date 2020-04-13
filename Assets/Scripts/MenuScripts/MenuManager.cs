d9bb10d5f3355df66083734120a233147bc0305a
Twonny98 <42622799+Twonny98@users.noreply.github.com>
2020-04-13 11:44:10 -0400

    delete button bug fix

--- Assets/Scripts/MenuScripts/MenuManager.cs
+++ Assets/Scripts/MenuScripts/MenuManager.cs
@@ -73,7 +73,7 @@ public class MenuManager : MonoBehaviour
         if (File.Exists(path))
         {
             File.Delete(path);
-            SceneManager.LoadScene("Menu");
+            SceneManager.LoadScene("MenuPostIntro");
         }
         else
         {
