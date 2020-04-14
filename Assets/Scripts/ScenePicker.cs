328ea68bfcd9f04676fd4e588f570e91eaed820e
Twonny98 <42622799+Twonny98@users.noreply.github.com>
2020-04-14 12:21:17 -0400

    debug level select tool added

---
+++ Assets/Scripts/ScenePicker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePicker : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            SceneManager.LoadScene("MenuPostIntro");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene("levelOneSprint4");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene("levelTwoSprint4");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SceneManager.LoadScene("levelThreeSprint4");
+        }
+       
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SceneManager.LoadScene("levelFourSprint4");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            SceneManager.LoadScene("levelFiveSprint4");
+        }
+    }
+}
