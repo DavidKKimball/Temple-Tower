4ec3e6426c9fd5d2275981b01aa739845a60d5b7
DK <davidkkimball@gmail.com>
2020-04-15 12:06:44 -0400

    updates

--- Assets/Scripts/stairFixer.cs
+++ Assets/Scripts/stairFixer.cs
@@ -7,13 +7,35 @@ public class stairFixer : MonoBehaviour
     public PhysicMaterial originalPhys;
     public PhysicMaterial stairPhys;
     public Collider player;
+    private GameObject playerMaster;
+    private Movement scriptHolder;
 
+    void Awake()
+    {
+        playerMaster = GameObject.Find("MilesNewWorking");
+        scriptHolder = playerMaster.GetComponent<Movement>();
+    }
+    void Update()
+    {
+        if (scriptHolder.speed < 2)
+        {
+            scriptHolder.speed = 3.7f;
+        }
+    }
     void OnTriggerStay(Collider other) 
     {
         if (other.gameObject.tag == "Player")            
         {
             //Debug.Log("hit");
             player.material = stairPhys;
+            if (scriptHolder.isRolling == false || scriptHolder.isJumping == false || scriptHolder.justJumped== false )
+            {
+                scriptHolder.speed = 2.0f;
+            }
+            else if (scriptHolder.isRolling == true || scriptHolder.isJumping == true || scriptHolder.justJumped == true)
+            {
+                scriptHolder.speed = 7.4f;
+            }
         }
     }
     void OnTriggerExit(Collider other) 
@@ -21,6 +43,7 @@ public class stairFixer : MonoBehaviour
         if (other.gameObject.tag == "Player")            
         {
             player.material = originalPhys;
+            scriptHolder.speed = 3.7f;
         }
     }
 }
