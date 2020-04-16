020aba8f6eff776d6ed058127619238fb9eefec2
DK <davidkkimball@gmail.com>
2020-04-16 13:43:03 -0400

    changes

---
+++ Assets/Scripts/boulderScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boulderScript : MonoBehaviour
+{
+    public GameObject player;
+    public Animator boulderanim;
+    public GameObject target;
+    public int manualDistance = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float playerDistance = Vector3.Distance(target.transform.position, player.transform.position);
+        Debug.Log(playerDistance);
+        if(playerDistance > 13 && manualDistance < 25)
+        {
+            boulderanim.speed = 1.2f;
+            Debug.Log("miles too far catching up...");
+        }
+        if(playerDistance >= 25)
+        {
+            boulderanim.speed = 2.0f;
+            Debug.Log("miles way too far catching up...");
+        }
+        if(playerDistance < 13 && playerDistance >=5)
+        {
+            boulderanim.speed = 0.5f;
+            Debug.Log("miles too close slowing down...");
+        }
+        if(playerDistance >= 2 && playerDistance <= 5)
+        {
+            boulderanim.speed = 1f;
+            Debug.Log("good distance");
+        }             
+    }        
+}
---
+++ Assets/Scripts/playOnAwakeLevelFour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playOnAwakeLevelFour : MonoBehaviour
+{
+    public Animator letterbox;
+    public Animator hud;
+    public Animator boulder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        letterbox.Play("FadeIn");
+        hud.Play("HUDSlideInIdleForVoiceOver");
+        boulder.Play("BoulderLevel4");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
