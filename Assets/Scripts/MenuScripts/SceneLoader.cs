b8d4c12fabd553a11400272c1c2fa404525ba478
DK <davidkkimball@gmail.com>
2020-04-15 21:00:53 -0400

    new intro

--- Assets/Scripts/MenuScripts/SceneLoader.cs
+++ Assets/Scripts/MenuScripts/SceneLoader.cs
@@ -5,10 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public Animator anim1;
+    public Animator anim2;
 
     // Update is called once per frame
     public void RunGame()
     {
-        SceneManager.LoadScene("Loading");
+        //SceneManager.LoadScene("Loading");
+        anim1.Play("newGameStart");
+        anim2.Play("menuFadeOut");
     }
 }
---
+++ Assets/Scripts/playOnAwakeLevel5.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playOnAwakeLevel5 : MonoBehaviour
+{
+    public Animator letterbox;
+    public Animator hud;
+    // Start is called before the first frame update
+    void Start()
+    {
+        letterbox.Play("FadeIn");
+        hud.Play("HUDSlideInIdleForVoiceOver");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
