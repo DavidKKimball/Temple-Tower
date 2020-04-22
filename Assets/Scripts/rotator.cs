0c76c7b80bed92c844f4d856967f23cfa73e9924
DK <davidkkimball@gmail.com>
2020-04-22 12:18:55 -0400

    improved boulder level 2

--- Assets/Scripts/rotator.cs
+++ Assets/Scripts/rotator.cs
@@ -2,9 +2,21 @@
 using System.Collections;
 
 public class rotator : MonoBehaviour {
+    public bool stopBall = false;
 
     void Update () 
     {
+        if (stopBall == true)
+        {
+            return;
+        }
+        if (stopBall == false )
+        {
         transform.Rotate (new Vector3 (0, 0, -120) * Time.deltaTime);
+        }
+    }
+    public void stopBallRotation()
+    {
+        stopBall = true;
     }
 }
